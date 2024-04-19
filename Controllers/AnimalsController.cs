f7fa748c620774c458f94b5ca7251e3e1e4577c0
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-19 18:08:01 +0200

    initial commit

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Tutorial5.Models;
+using Tutorial5.Models.DTOs;
+
+namespace Tutorial5.Controllers;
+
+[ApiController]
+// [Route("api/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        // Otwieramy połączenie
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        
+        // Definiujemy command
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal";
+        
+        // Wykonanie zapytania
+        var reader = command.ExecuteReader();
+
+        List<Animal> animals = new List<Animal>();
+
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+        
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal)
+            });
+        }
+
+        // var animal = _repository.GetAnimals();
+
+        return Ok(animals);
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimal(AddAnimal addAnimal)
+    {
+        // Otwieramy połączenie
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        
+        // Definiujemy command
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "INSERT INTO Animal VALUES (@animalName, '','' ,'')";
+        command.Parameters.AddWithValue("@animalName", addAnimal.Name);
+        
+        // Wykonanie zapytania
+        command.ExecuteNonQuery();
+
+        // _repository.AddAnimal(addAnimal);
+        
+        return Created("", null);
+    }
+}
---
+++ Models/Animal.cs
@@ -0,0 +1,7 @@
+﻿namespace Tutorial5.Models;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string Name { get; set; }
+}
