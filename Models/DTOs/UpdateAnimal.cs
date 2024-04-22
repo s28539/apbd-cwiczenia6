60276c63c0b37a2e1799821335399d4b4d28945e
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-22 23:56:18 +0200

    add [Required] header

--- Models/DTOs/UpdateAnimal.cs
+++ Models/DTOs/UpdateAnimal.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace Tutorial5.Models.DTOs;
 
@@ -6,12 +8,15 @@ public class UpdateAnimal
 {
     [MinLength(3)]
     [MaxLength(200)]
+    [Required]    
     public string Name { get; set; }
     [MaxLength(200)]
     public string? Description { get; set; }
     [MaxLength(200)]
+    [Required]    
     public string Category { get; set; }
     [MaxLength(200)]
+    [Required]    
     public string Area { get; set; }
 
     public UpdateAnimal(string name, string? description, string category, string area)
