6f4dbc1ac8d5f898343b378eba6c93e1c88753fa
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-22 23:14:07 +0200

    refactor AnimalsController.cs

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using Tutorial5.Models;
 using Tutorial5.Models.DTOs;
+using Tutorial5.Repositories;
 
 namespace Tutorial5.Controllers;
 
@@ -17,35 +18,36 @@ public class AnimalsController : ControllerBase
     }
     
     [HttpGet]
-    public IActionResult GetAnimals()
+    public IActionResult GetAnimals([FromQuery] string orderBy = null)
     {
-        // Otwieramy połączenie
-        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
-        connection.Open();
+        var animals = PostgreSQLRepository
+            .GetRepository(_configuration.GetConnectionString("Default"))
+            .AsEnumerable();
         
-        // Definiujemy command
-        using SqlCommand command = new SqlCommand();
-        command.Connection = connection;
-        command.CommandText = "SELECT * FROM Animal";
-        
-        // Wykonanie zapytania
-        var reader = command.ExecuteReader();
-
-        List<Animal> animals = new List<Animal>();
-
-        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
-        int nameOrdinal = reader.GetOrdinal("Name");
-        
-        while (reader.Read())
+        if (!string.IsNullOrEmpty(orderBy))
         {
-            animals.Add(new Animal()
+            switch (orderBy)
             {
-                IdAnimal = reader.GetInt32(idAnimalOrdinal),
-                Name = reader.GetString(nameOrdinal)
-            });
+                case "Name":
+                    animals = animals.OrderBy(a => a.Name);
+                    break;
+                case "Description":
+                    animals = animals.OrderBy(a => a.Desription);
+                    break;
+                case "Category":
+                    animals = animals.OrderBy(a => a.Category);
+                    break;
+                case "Area":
+                    animals = animals.OrderBy(a => a.Area);
+                    break;
+                default:
+                    return BadRequest("Invalid input OrderBy sorry:/");
+            }
         }
-
-        // var animal = _repository.GetAnimals();
+        else
+        {
+             animals = animals.OrderBy(a => a.Name);
+        }
 
         return Ok(animals);
     }
@@ -53,20 +55,7 @@ public class AnimalsController : ControllerBase
     [HttpPost]
     public IActionResult AddAnimal(AddAnimal addAnimal)
     {
-        // Otwieramy połączenie
-        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
-        connection.Open();
-        
-        // Definiujemy command
-        using SqlCommand command = new SqlCommand();
-        command.Connection = connection;
-        command.CommandText = "INSERT INTO Animal VALUES (@animalName, '','' ,'')";
-        command.Parameters.AddWithValue("@animalName", addAnimal.Name);
-        
-        // Wykonanie zapytania
-        command.ExecuteNonQuery();
-
-        // _repository.AddAnimal(addAnimal);
+        PostgreSQLRepository.AddAnimal(_configuration.GetConnectionString("Default"),addAnimal);
         
         return Created("", null);
     }
