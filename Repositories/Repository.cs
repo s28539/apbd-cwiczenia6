2846531dc36c391408cbd36928a7d320da56086e
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-22 23:55:58 +0200

    add DependencyInjection

--- Repositories/Repository.cs
+++ Repositories/Repository.cs
@@ -1,8 +1,17 @@
+using Npgsql;
+using Tutorial5.Models;
+using Tutorial5.Models.DTOs;
+
 namespace Tutorial5.Repositories;
 
 public interface Repository
 {
-    public void GetRepository()
-    {
-    }
+    public  List<Animal> GetRepository(string connectionString);
+
+    public  void AddAnimal(string connectionString, AddAnimal addAnimal);
+
+    public  void UpdateAnimal(string connectionString, int idAnimal, UpdateAnimal updateAnimal);
+
+    public void DeleteAnimal(string connectionString, int idAnimal);
+
 }
