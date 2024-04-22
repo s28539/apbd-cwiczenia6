c377b7467aee037243481757400382cadfbb0669
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-22 23:13:44 +0200

    add constructor and fields

--- Models/DTOs/AddAnimal.cs
+++ Models/DTOs/AddAnimal.cs
@@ -10,4 +10,16 @@ public class AddAnimal
     public string Name { get; set; }
     [MaxLength(200)]
     public string? Description { get; set; }
+    [MaxLength(200)]
+    public string Category { get; set; }
+    [MaxLength(200)]
+    public string Area { get; set; }
+
+    public AddAnimal(string name, string? description, string category, string area)
+    {
+        Name = name;
+        Description = description;
+        Category = category;
+        Area = area;
+    }
 }
