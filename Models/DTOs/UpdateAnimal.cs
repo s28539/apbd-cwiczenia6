d6bea308bfcd1b996457d905314018743364b0b7
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-22 23:43:08 +0200

    crate UpdateAnimal.cs

---
+++ Models/DTOs/UpdateAnimal.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial5.Models.DTOs;
+
+public class UpdateAnimal
+{
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [MaxLength(200)]
+    public string? Description { get; set; }
+    [MaxLength(200)]
+    public string Category { get; set; }
+    [MaxLength(200)]
+    public string Area { get; set; }
+
+    public UpdateAnimal(string name, string? description, string category, string area)
+    {
+        Name = name;
+        Description = description;
+        Category = category;
+        Area = area;
+    }
+}
